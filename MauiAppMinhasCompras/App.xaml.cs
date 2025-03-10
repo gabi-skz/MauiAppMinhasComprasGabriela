e61feb301c4f9bf3747c973c8ce3d9dc52ff1559
RAFAELA OLIVEIRA <rafaela.oliveira308@etec.sp.gov.br>
2025-03-09 21:52:22 -0300

    Modificação para a Agenda 3

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db; // tornando o banco de dados do sqlite disponivel
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine( // o caminho até o banco de dados b3
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -2,8 +2,21 @@ namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	public ListaProduto()
-	{
-		InitializeComponent();
-	}
+    public ListaProduto()
+    {
+        InitializeComponent();
+    }
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e) //no item clicado oq acontece
+    {
+        try
+        {
+            Navigation.PushAsync(new Views.NovoProduto()); //fazendo a navegação entre as telas, a lista de produtos com a pagina novo produto
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
