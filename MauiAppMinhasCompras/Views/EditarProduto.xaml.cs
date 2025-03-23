44d2037cb134de449669cae94bd5222daaeafa73
GABRIELA OLIVEIRA <rafaela.oliveira308@etec.sp.gov.br>
2025-03-23 16:13:51 -0300

    Modificação para a Agenda 5

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,9 +1,34 @@
 namespace MauiAppMinhasCompras.Views;
+using MauiAppMinhasCompras.Models;
 
 public partial class EditarProduto : ContentPage
 {
-	public EditarProduto()
-	{
-		InitializeComponent();
-	}
+    public EditarProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e) //toda vez que eu crio um novo manipulador ele cria automatico
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Update(p);
+            await DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+            await Navigation.PopAsync(); //volta a tela de origem
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK"); //await seria sendo aguardado
+        }
+    }
 }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -16,10 +16,20 @@ public partial class ListaProduto : ContentPage
 
     protected async override void OnAppearing()
     {
-        List<Produto> tmp = await App.Db.GetAll();
+        try
+        {
+            lista.Clear();
 
-        tmp.ForEach(i => lista.Add(i));
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
+
 
     private void ToolbarItem_Clicked(object sender, EventArgs e) //no item clicado oq acontece
     {
@@ -35,15 +45,21 @@ public partial class ListaProduto : ContentPage
     }
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-        string q = e.NewTextValue;
+        try
+        {
+            string q = e.NewTextValue;
 
-        lista.Clear();
+            lista.Clear();
 
-        List<Produto> tmp = await App.Db.Search(q);
+            List<Produto> tmp = await App.Db.Search(q);
 
-        tmp.ForEach(i => lista.Add(i));
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK"); // ele quer ser await por ser assincrono, diferente do outro, ele espera kkk
+        }
     }
-
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
         double soma = lista.Sum(i => i.Total);
@@ -53,8 +69,38 @@ public partial class ListaProduto : ContentPage
         DisplayAlert("Total dos Produtos", msg, "OK");
     }
 
-    private void MenuItem_Clicked(object sender, EventArgs e)
+    private async void MenuItem_Clicked(object sender, EventArgs e)
     {
-
+        try
+        {
+            MenuItem selecionado = sender as MenuItem; //sempre que clicar vai mostrar qual foi selecionado
+            Produto p = selecionado.BindingContext as Produto; // vai ser o item em questao que selecionou
+            bool confirm = await DisplayAlert(
+                "Tem certeza?", $"Remover {p.Descricao}?", "Sim", "Não"); //cria um alerta para ter certeza de remover com confirma e nao confirma
+            if (confirm)
+            {
+                await App.Db.Delete(p.Id); //tira ele do banco de dados
+                lista.Remove(p); //remove da lista
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto; //é uma rede objeto
+            Navigation.PushAsync(new Views.EditarProduto //vai para a pagina de editar produto
+            {
+                BindingContext = p,
+            });
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
